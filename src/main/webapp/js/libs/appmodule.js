0cf4de835d3ffe692f64ca9bb883959306baab9e
hectorateci <hector.cadavid@escuelaing.edu.co>
2015-04-23 15:53:14 -0500

    Update appmodule.js

--- src/main/webapp/js/libs/appmodule.js
+++ src/main/webapp/js/libs/appmodule.js
@@ -1,19 +1,7 @@
 (function () {
     var app = angular.module('modone', ['ngRoute']);
 
-    app.controller('controlenvio',
-            function($http,$scope){
-                $scope.destinatario="NN";
-                $scope.mensajes="-------";
-                
-                $scope.envio="Escriba aca";
-                
-                this.enviar=function(){
-                    alert('click');
-                };
-                        
-            }
-    );
+    
    
 
 })();
